2569071c1b3c4bca3561278e4248d8b268eb74a7
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2021-06-10 12:23:01 +0530

    add styled in shows components

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -6,6 +6,7 @@ import ShowMainData from '../components/shows/ShowMainData';
 import Details from '../components/shows/Details';
 import Seasons from '../components/shows/Seasons';
 import Cast from '../components/shows/Cast';
+import { InfoBlock, ShowPageWrapper } from '../components/shows/Show.styled';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -65,7 +66,7 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -74,25 +75,25 @@ const Show = () => {
         tags={show.genres}
       />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
